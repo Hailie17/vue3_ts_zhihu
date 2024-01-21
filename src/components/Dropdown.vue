2693dae335237d9c8b78edcadd2542d8198c7ad8
Hailie17 <192151452@qq.com>
2024-01-21 15:59:07 +0800

    封装dropdown item 组件

--- src/components/Dropdown.vue
+++ src/components/Dropdown.vue
@@ -3,8 +3,7 @@
     <!-- .prevent 防止a链接的默认行为 -->
     <a href="#" class="btn btn-outline-light my-2 dropdown-toggle" @click.prevent="toggleOpen"> {{ title }}</a>
     <ul class="dropdown-menu" :style="{display: 'block'}" v-if="isOpen">
-      <li><a class="dropdown-item" href="#">新建文章</a></li>
-      <li><a class="dropdown-item" href="#">编辑资料</a></li>
+      <slot></slot>
     </ul>
   </div>
 </template>
