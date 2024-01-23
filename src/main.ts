61f9b3d587cc5f0eafeec07541c2acb396a3fae5
Hailie17 <192151452@qq.com>
2024-01-23 17:16:08 +0800

    方法：接口配置跨域，避开icode

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { createApp } from 'vue'
-
 import router from './router'
 import store from './store'
 import App from './App.vue'
+import axios from 'axios'
 
+axios.get('http://api.vikingship.xyz/api/columns').then((res) => {
+  console.log(res)
+})
 const app = createApp(App)
 app.use(router)
 app.use(store)
