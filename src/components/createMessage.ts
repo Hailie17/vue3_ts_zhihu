c4cd8665742d180a4895df002b77aa6c7d721e95
Hailie17 <192151452@qq.com>
2024-01-25 01:25:00 +0800

    方法：使用h函数改造message方法

--- src/components/createMessage.ts
+++ src/components/createMessage.ts
@@ -1,17 +1,17 @@
-import { createApp } from 'vue'
+import { h, render } from 'vue'
 import Message from './Message.vue'
 export type MessageType = 'success' | 'error' | 'default'
 
 const createMessage = (message: string, type: MessageType, timeout = 2000) => {
-  const messageInstance = createApp(Message, {
+  const messageVnode = h(Message, {
     message,
     type
   })
   const mountNode = document.createElement('div')
   document.body.appendChild(mountNode)
-  messageInstance.mount(mountNode) // 挂载到节点上
+  render(messageVnode, mountNode) // 挂载节点
   setTimeout(() => {
-    messageInstance.unmount()
+    render(null, mountNode) // 卸载节点
     document.body.removeChild(mountNode)
   }, timeout)
 }
