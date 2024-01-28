18eacb3584a0192a2fc51a71d989ec46b304c0e8
Hailie17 <192151452@qq.com>
2024-01-29 01:36:06 +0800

    数组转对象方法，对象转数组方法

--- src/helper.ts
+++ src/helper.ts
@@ -29,3 +29,20 @@ export function beforeUploadCheck (file: File, condition: CheckCondition) {
     error
   }
 }
+interface TestProps {
+  _id?: string;
+  name: string;
+}
+const testData: TestProps[] = [{ _id: '1', name: 'a' }, { _id: '2', name: 'b' }]
+
+export const arrToObj = <T extends { _id?: string }>(arr: Array<T>) => {
+  return arr.reduce((prev, current) => {
+    if (current._id) {
+      prev[current._id] = current
+    }
+    return prev
+  }, {} as {[key: string]: T}) // 类型断言：把 {} 断言成 {[key: string]: T}
+}
+export const objToArr = <T>(obj: { [key: string]: T }) => {
+  return Object.keys(obj).map(key => obj[key])
+}
