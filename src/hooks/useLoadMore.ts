8f54b05871b7b36e9bce383e6df4ed486b6390e0
Hailie17 <192151452@qq.com>
2024-01-31 00:15:09 +0800

    useLoadMore hooks 编码

--- src/hooks/useLoadMore.ts
+++ src/hooks/useLoadMore.ts
@@ -1,4 +1,4 @@
-import { useStore } from "vuex"
+import { useStore } from 'vuex'
 import { ref, computed, ComputedRef } from 'vue'
 
 interface LoadParams {
@@ -6,5 +6,23 @@ interface LoadParams {
   pageSize: number;
 }
 export const useLoadMore = (actionName: string, total: ComputedRef<number>, params: LoadParams = { currentPage: 2, pageSize: 5 }) => {
-
+  const store = useStore()
+  const currentPage = ref(params.currentPage)
+  const requestParams = {
+    currentPage: currentPage.value,
+    pageSize: params.pageSize
+  }
+  const loadMorePage = () => {
+    store.dispatch(actionName, requestParams).then(() => {
+      currentPage.value++
+    })
+  }
+  const isLastPage = computed(() => {
+    return Math.ceil(total.value / params.pageSize) === currentPage.value
+  })
+  return {
+    loadMorePage,
+    isLastPage,
+    currentPage
+  }
 }
