613ffaf90d5e0893f653e432f00f6477f11344bb
Hailie17 <192151452@qq.com>
2024-01-31 23:33:44 +0800

    分页缓存

--- src/store.ts
+++ src/store.ts
@@ -47,7 +47,7 @@ export interface GlobalDataProps {
   error: GlobalErrorProps;
   token: string;
   loading: boolean;
-  columns: { data: ListProps<ColumnProps>; isLoaded: boolean, total: number };
+  columns: { data: ListProps<ColumnProps>; currentPage: number, total: number };
   posts: { data: ListProps<PostProps>; loadedColumns: string[] };
   user: UserProps;
 }
@@ -66,7 +66,7 @@ const store = createStore<GlobalDataProps>({
     error: { status: false },
     token: localStorage.getItem('token') || '',
     loading: false,
-    columns: { data: {}, isLoaded: false, total: 0 },
+    columns: { data: {}, currentPage: 0, total: 0 },
     posts: { data: {}, loadedColumns: [] },
     user: { isLogin: false }
   },
@@ -76,11 +76,11 @@ const store = createStore<GlobalDataProps>({
     },
     fetchColumns (state, rawData) {
       const { data } = state.columns
-      const { list, count } = rawData.data
+      const { list, count, currentPage } = rawData.data
       state.columns = {
         data: { ...data, ...arrToObj(list) },
         total: count,
-        isLoaded: true
+        currentPage: currentPage * 1 // 类型转换，如果是string就转换为数字
       }
     },
     fetchColumn (state, rawData) {
@@ -127,7 +127,9 @@ const store = createStore<GlobalDataProps>({
       // if (!state.columns.isLoaded) {
       //   return asyncAndCommit('/columns', 'fetchColumns', commit)
       // }
-      return asyncAndCommit(`/columns?currentPage=${currentPage}&pageSize=${pageSize}`, 'fetchColumns', commit)
+      if (state.columns.currentPage < currentPage) {
+        return asyncAndCommit(`/columns?currentPage=${currentPage}&pageSize=${pageSize}`, 'fetchColumns', commit)
+      }
     },
     fetchColumn ({ state, commit }, cid) {
       if (!state.columns.data[cid]) {
